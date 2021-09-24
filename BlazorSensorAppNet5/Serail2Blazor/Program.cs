7be4b2c230056c39e9c9dd6f2e0bd0872e66f0d3
David Jones <davidjones@sportronics.com.au>
2021-09-24 14:06:21 +1000

    Serial app can now run in unattended mode. Set Auto to true in appsetting.json in teh app folder.

--- BlazorSensorAppNet5/Serail2Blazor/Program.cs
+++ BlazorSensorAppNet5/Serail2Blazor/Program.cs
@@ -15,6 +15,7 @@ using System.Configuration;
 //rEF: https://stackoverflow.com/questions/65110479/how-to-get-values-from-appsettings-json-in-a-console-application-using-net-core
 using Microsoft.Extensions.Configuration;
 using System.IO;
+using System.Collections.Generic;
 // NuGet packages:
 // Microsoft.Extensions.Configuration.Binder
 // Microsoft.Extensions.Configuration.Jso
@@ -32,6 +33,7 @@ namespace SerialBlazor
         static bool isFirstRead = true;
         static string comport = "COM4";
         static int baudrate = 9600;
+        static bool auto = false;
         static void Main(string[] args)
         {
             var builder = new ConfigurationBuilder()
@@ -41,26 +43,42 @@ namespace SerialBlazor
             IConfiguration configuration = builder.Build();
             Settings settings = configuration.GetSection("AppSettings").Get<Settings>();
 
-            _host = $@"{settings.HOST}:{settings.PORT}/";
-            delay = settings.DELAY_SEC;
-            comport = settings.COM_PORT;
-            baudrate = settings.BAUD_RATE;
+            _host = $@"{settings.Host}:{settings.Port}/";
+            delay = settings.Delay_Secs;
+            comport = settings.ComPort;
+            baudrate = settings.BaudRate;
+            auto = settings.Auto;
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello IoT Nerd!");
             // Get a list of serial port names.
             string[] ports = SerialPort.GetPortNames();
-            Console.WriteLine("The following serial ports were found:");             //
-            // Display each port name to the console.
-            foreach(string _port in ports)
-            {                
-                Console.WriteLine(_port);             
-            }
-
-            Console.Write("Port no: (Default COM4, x to use all defaults) ");
-            string port = Console.ReadLine();
-            if (port.ToLower() != "x")
+            var lst = new List<string>(ports);
+            if ((!auto) || (!lst.Contains(comport)))
             {
-                if (!string.IsNullOrEmpty(port))
+                if (!lst.Contains(comport))
+                    comport = "";
+                Console.WriteLine("The following serial ports were found:");                                                                                          
+                // Display each port name to the console.
+                foreach (string _port in ports)
+                {
+                    Console.WriteLine(_port);
+                }
+
+                string port = "";
+                bool cont;
+                do
+                {
+                    if (comport =="")
+                        Console.Write($"Port no: (No default) ");
+                    else
+                        Console.Write($"Port no: (Default {comport}) ");
+                    port = Console.ReadLine();
+                    cont = !lst.Contains(port);
+                    // Can just press return for defaults.
+                    if ( (string.IsNullOrEmpty(port)) && (comport != ""))
+                        cont = false;
+                } while (cont);
+                if (!string.IsNullOrEmpty(port)) 
                     comport = port;
 
                 Console.Write($"baudrate: (Default: {baudrate}) ");
@@ -68,22 +86,22 @@ namespace SerialBlazor
                 if (!string.IsNullOrEmpty(newbaudrate))
                 {
                     if (!int.TryParse(newbaudrate, out baudrate))
-                        baudrate = settings.BAUD_RATE;
+                        baudrate = settings.BaudRate;
                 }
 
                 Console.Write($"Time (in sec) between reads: (Default:{delay}) ");
                 string secs = Console.ReadLine();
                 if (!int.TryParse(secs, out delay))
                 {
-                    delay = settings.DELAY_SEC;
+                    delay = settings.Delay_Secs;
                 }
 
                 Console.Write($@"Blazor Server URL:Port: (Default:{_host} ) ");
                 string newhost = Console.ReadLine();
                 if (!string.IsNullOrEmpty(newhost))
                     _host = newhost;
+                
             }
-            else
 
             if (_host[_host.Length - 1] != '/')
                 _host += "/";
@@ -94,22 +112,27 @@ namespace SerialBlazor
             _serialPort.ReadTimeout = settings.ReadTimeout;
             _serialPort.WriteTimeout = settings.WriteTimeout;
             _serialPort.Open();
-            Console.WriteLine("Opened the port.");
-
-            Console.Write("Press enter when Server is up.");
-            Console.ReadLine();
-            Console.WriteLine();
-            Console.WriteLine("Use Azure IoT Explorer to display Telemetry sent:");
-            Console.WriteLine(@"https://docs.microsoft.com/en-us/azure/iot-fundamentals/howto-use-iot-explorer");
-            Console.WriteLine();
-            bool cont = true;
-            while (cont)
+            if (_serialPort.IsOpen)
             {
-                Signal();
-                Read().GetAwaiter();
+
+                Console.WriteLine("Opened the port.");
+
+                Console.Write("Press enter when Server is up.");
+                Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine("Use Azure IoT Explorer to display Telemetry sent:");
+                Console.WriteLine(@"https://docs.microsoft.com/en-us/azure/iot-fundamentals/howto-use-iot-explorer");
+                Console.WriteLine();
+                bool cont = true;
+                while (cont)
+                {
+                    Signal();
+                    Read().GetAwaiter();
+                }
+                _serialPort.Close();
             }
-            _serialPort.Close();
-            Console.ReadLine();        
+            else
+                Console.WriteLine("Serial port {0} failed to open.", comport);      
         }
 
         public static void Signal()
@@ -117,7 +140,10 @@ namespace SerialBlazor
             if(isFirstRead)
             {
                 isFirstRead = false;
-                Thread.Sleep(1000);
+                if (auto)
+                    Thread.Sleep(30000);
+                else
+                    Thread.Sleep(3000);
             }
             else
              Thread.Sleep(delay*1000);
@@ -174,11 +200,12 @@ namespace SerialBlazor
 
     public class Settings
     {
-        public string COM_PORT { get; set; }
-        public int BAUD_RATE { get; set; }
-        public int DELAY_SEC { get; set; }
-        public string HOST { get; set; }
-        public uint PORT { get; set; }
+        public bool Auto {get; set;}
+        public string ComPort { get; set; }
+        public int BaudRate { get; set; }
+        public int Delay_Secs { get; set; }
+        public string Host { get; set; }
+        public uint Port { get; set; }
         public int WriteTimeout { get; set; }
         public int ReadTimeout {get; set;}
 
